ef54857c3c66f2b80d72f4d9888905c9edd1b35c
YasmineSaad79 <saad9055@gmail.com>
2026-03-16 22:57:18 +0200

    fix: use flutter 3.24.0

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-# Install Flutter
-git clone https://github.com/flutter/flutter.git -b stable --depth 1 /tmp/flutter
+# Install Flutter 3.24.0 (compatible with google_fonts 4.x)
+git clone https://github.com/flutter/flutter.git -b 3.24.0 --depth 1 /tmp/flutter
 export PATH="$PATH:/tmp/flutter/bin"
 
 # Build
